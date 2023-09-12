8d0582f551d82d4cffa0f65d00c2a71cdb731484
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-09-12 08:15:57 +0330

    working

--- bot/controllers/price/price.controllers.js
+++ bot/controllers/price/price.controllers.js
@@ -1,5 +1,11 @@
 import { API } from "$bot/api/index.js";
 
+function getEmojiFlag(value) {
+  const code = value.split("_")[1].substring(0, 2);
+
+  return code || "🏳️";
+}
+
 export const GOLD = async (ctx) => {
   try {
     const { data } = await API.get("price/gold");
@@ -16,7 +22,9 @@ export const CURRENCY = async (ctx) => {
   try {
     const { data } = await API.get("price/currency");
 
-    const messages = data.map((item) => `${item.title}: ${item.price}`);
+    const messages = data.map(
+      (item) => `${getEmojiFlag(item.value)} ${item.title}: ${item.price}`
+    );
 
     await ctx.replyWithMarkdown(messages.join("\n"));
   } catch (error) {
